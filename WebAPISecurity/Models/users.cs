68ae6729f2801383455afcf286af61be524717b2
HarshuVenkat-Dev <94186416+HarshuVenkat-Dev@users.noreply.github.com>
2022-02-22 22:37:51 +0530

    Add files via upload

---
+++ WebAPISecurity/Models/users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPISecurity.Models
+{
+    public class users
+    {
+        public int id;
+        public string name;
+        public string email;
+        public string gender;
+        public string status;
+
+        public static implicit operator List<object>(users v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
